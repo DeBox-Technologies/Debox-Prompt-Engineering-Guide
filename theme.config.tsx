c4df917cb9538198936c9b10b2e9a8231dddb382
MichaelHoughtonDeBox <michael@deboxfinancial.com>
2024-07-01 14:33:54 +0100

    initial files and changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,45 +4,43 @@ import { useConfig } from 'nextra-theme-docs'
 import { Pre } from './components/pre'
 
 const config: DocsThemeConfig = {
+  // Add this line to set the primary color
+  primaryHue: 170, // This is approximately the hue of #19A890
+
   logo: (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 206 246" fill="none">
-        <circle cx="40" cy="40" r="40" fill="currentColor"/>
-        <circle cx="40" cy="206" r="40" fill="currentColor"/>
-        <circle cx="166" cy="120" r="40" fill="currentColor"/>
-      </svg>
       <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
-        Prompt Engineering Guide
+        Baobab Prompt Engineering Guide
       </span>
     </>
   ),
   i18n: [
     { locale: 'en', text: 'English' },
-    { locale: 'zh', text: '中文' },
-    { locale: 'jp', text: '日本語'},
-    { locale: 'pt', text: 'Português' },
-    { locale: 'it', text: 'Italian' },
-    { locale: 'tr', text: 'Türkçe' },
-    { locale: 'es', text: 'Español' },
-    { locale: 'fr', text: 'Français' },
-    { locale: 'kr', text: '한국어' },
-    { locale: `ca`, text: `Català` },
-    { locale: 'fi', text: 'Finnish' },
-    { locale: 'ru', text: 'Русский' },
-    { locale: 'de', text: 'Deutsch' },
-    { locale: 'ar', text: 'العربية' },
+    // { locale: 'zh', text: '中文' },
+    // { locale: 'jp', text: '日本語'},
+    // { locale: 'pt', text: 'Português' },
+    // { locale: 'it', text: 'Italian' },
+    // { locale: 'tr', text: 'Türkçe' },
+    // { locale: 'es', text: 'Español' },
+    // { locale: 'fr', text: 'Français' },
+    // { locale: 'kr', text: '한국어' },
+    // { locale: `ca`, text: `Català` },
+    // { locale: 'fi', text: 'Finnish' },
+    // { locale: 'ru', text: 'Русский' },
+    // { locale: 'de', text: 'Deutsch' },
+    // { locale: 'ar', text: 'العربية' },
   ],
   head: function UseHead() {
     const { title } = useConfig()
     return (
     <>
-      <title>{title ? title + ' | Prompt Engineering Guide': 'Prompt Engineering Guide'} </title>
+      <title>{title ? title + ' | Baobab Prompt Engineering Guide': 'Baobab Prompt Engineering Guide'} </title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Prompt Engineering Guide" />
       <meta property="og:description" content="A Comprehensive Overview of Prompt Engineering" />
       <meta
           name="og:title"
-          content={title ? title + ' | Prompt Engineering Guide' : 'Prompt Engineering Guide'}
+          content={title ? title + ' | Baobab Prompt Engineering Guide' : 'Baobab Prompt Engineering Guide'}
         />
       <link rel="icon" href="/144-favicon.svg" type="image/svg+xml" />
 
@@ -54,19 +52,19 @@ const config: DocsThemeConfig = {
         />
     </>
   )},
-  project: {
-    link: 'https://github.com/dair-ai/Prompt-Engineering-Guide',
-  },
-  chat: {
-    link: 'https://discord.gg/FUyz9vPAwf',
-  },
-  docsRepositoryBase: 'https://github.com/dair-ai/Prompt-Engineering-Guide/tree/main/',
-  footer: {
-    text: 'Copyright © 2024 DAIR.AI',
-  },
-  search: {
-    placeholder: 'Search...',
-  },
+  // project: {
+  //   link: 'https://github.com/dair-ai/Prompt-Engineering-Guide',
+  // },
+  // chat: {
+  //   link: 'https://discord.gg/FUyz9vPAwf',
+  // },
+  // docsRepositoryBase: 'https://github.com/dair-ai/Prompt-Engineering-Guide/tree/main/',
+  // footer: {
+  //   text: 'Copyright © 2024 DAIR.AI',
+  // },
+  // search: {
+  //   placeholder: 'Search...',
+  // },
   components: {
     pre: Pre,
   },
